d6915f12884fcc214ce94e7228f27b4afad8647e
Dikanb <dikanb1502@gmail.com>
2025-04-28 17:21:25 +0300

    make build

---
+++ index.1f358318.js
@@ -0,0 +1,2 @@
+document.addEventListener("keydown",function(e){console.log(e.target.id),"last_phone"===e.target.id&&e.preventDefault()});
+//# sourceMappingURL=index.1f358318.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,7 @@
+"use strict";
+document.addEventListener("keydown", (event)=>{
+    console.log(event.target.id);
+    if (event.target.id === "last_phone") event.preventDefault();
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
